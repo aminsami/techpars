578e738913115dedb276b65594777727617c7846
aminsami <aminsami91@gmail.com>
2023-12-06 13:12:54 +0330

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,127 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title> Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header title="" />
+    <a href="httpshttps://www.storeboard.com/dibapart">click here</a>
+<a href="httpshttps://www.storeboard.com/alobag1">click here</a>
+<a href="httpshttps://www.storeboard.com/cooltech1">click here</a>
+<a href="httpshttps://lwccareers.lindsey.edu/profiles/3982983-cool-tech">click here</a>
+<a href="httpshttps://lwccareers.lindsey.edu/profiles/3983002-diba-part">click here</a>
+<a href="httpshttps://lwccareers.lindsey.edu/profiles/3983013-alo-bag">click here</a>
+<a href="httpshttps://www.link-your-site.com/member/cooltech.html">click here</a>
+<a href="httpshttps://www.link-your-site.com/member/dibapart.html">click here</a>
+<a href="httpshttps://www.link-your-site.com/listing/business/32621-alobag.html">click here</a>
+<a href="httpshttp://sanaat123.rozblog.com/">click here</a>
+<a href="httpshttps://web.notifyninja.com/alobag.com">click here</a>
+<a href="httpshttps://web.notifyninja.com/dibapart.com">click here</a>
+<a href="httpshttps://web.notifyninja.com/cooltech.cool">click here</a>
+<a href="httpshttps://profiles.delphiforums.com/n/pfx/profile.aspx?nav=profile&webtag=dfpprofile000&userId=1891204216">click here</a>
+<a href="httpshttps://sites.google.com/view/majaleh/home">click here</a>
+<a href="httpshttps://www.tumblr.com/blog/cooltechhvac12">click here</a>
+<a href="httpshttp://www.starjiwoo.com/renewal/?document_srl=636771&mid=freeboard&page=508&rnd=2747396#comment_2747396">click here</a>
+<a href="httpshttp://partdiba.toonblog.ir/">click here</a>
+<a href="httpshttps://sanat124.monoblog.ir/">click here</a>
+<a href="httpshttps://fancoiliran.blogsky.com/">click here</a>
+<a href="httpshttps://maps.google.com/url?sa=t&url=https%3A%2F%2Falobog.com%2F">click here</a>
+<a href="httpshttps://maps.google.com/url?sa=t&url=https%3A%2F%2Fcooltech.cool%2F">click here</a>
+<a href="httpshttps://maps.google.com/url?sa=t&url=https%3A%2F%2Fballoonchi.com%2F">click here</a>
+<a href="httpshttps://maps.google.com/url?sa=t&url=https%3A%2F%2Fdibapart.com%2F">click here</a>
+<a href="httpshttps://www.youtube.com/redirect?q=alobag.com">click here</a>
+<a href="httpshttps://www.youtube.com/redirect?q=cooltech.cool">click here</a>
+<a href="httpshttps://www.youtube.com/redirect?q=dibapart.com">click here</a>
+<a href="httpshttps://www.youtube.com/redirect?q=balloonchi.com">click here</a>
+<a href="httpshttps://paper.dropbox.com/ep/redirect/external-link?url=http%3A%2F%2Falobag.com&hmac=XC076%2FyQdixV26vHEOdeA%2Bm6hCzJ%2B1LuaL5SrFmv5QI%3D">click here</a>
+<a href="httpshttps://paper.dropbox.com/ep/redirect/external-link?url=https%3A%2F%2Fdibapart.com&hmac=XC076%2FyQdixV26vHEOdeA%2Bm6hCzJ%2B1LuaL5SrFmv5QI%3D">click here</a>
+<a href="httpshttps://paper.dropbox.com/ep/redirect/external-link?url=https%3A%2F%2Fcooltech.cool&hmac=XC076%2FyQdixV26vHEOdeA%2Bm6hCzJ%2B1LuaL5SrFmv5QI%3D">click here</a>
+<a href="httpshttps://paper.dropbox.com/ep/redirect/external-link?url=https%3A%2F%2Fballoonchi.com&hmac=XC076%2FyQdixV26vHEOdeA%2Bm6hCzJ%2B1LuaL5SrFmv5QI%3D">click here</a>
+<a href="httpshttps://medium.com/r?url=https%3A%2F%2Falobag.com%2F">click here</a>
+<a href="httpshttps://medium.com/r?url=https%3A%2F%2Fballoonchi.com%2F">click here</a>
+<a href="httpshttps://medium.com/r?url=https%3A%2F%2Fdibapart.com%2F">click here</a>
+<a href="httpshttps://medium.com/r?url=https%3A%2F%2Fcooltech.cool%2F">click here</a>
+<a href="httpshttp://cr.naver.com/redirect-notification?u=https%3A%2F%2Falobag.com">click here</a>
+<a href="httpshttps://cr.naver.com/redirect-notification?u=https%3A%2F%2Fcooltech.cool">click here</a>
+<a href="httpshttp://cr.naver.com/redirect-notification?u=https%3A%2F%2Fdibapart.com">click here</a>
+<a href="httpshttps://cr.naver.com/redirect-notification?u=https%3A%2F%2Fballoonchi.com">click here</a>
+<a href="httpshttps://pb.lib.berkeley.edu/xtf/servlet/org.cdlib.xtf.dynaXML.DynaXML?source=/BITAGAP/Display/9775BITAGAP.Work.xml&style=Work.xsl&gobk=https%3A%2F%2Fdibapart.com">click here</a>
+<a href="httpshttps://pb.lib.berkeley.edu/xtf/servlet/org.cdlib.xtf.dynaXML.DynaXML?source=/BITAGAP/Display/9775BITAGAP.Work.xml&style=Work.xsl&gobk=https%3A%2F%2Fcooltech.cool">click here</a>
+<a href="httpshttps://pb.lib.berkeley.edu/xtf/servlet/org.cdlib.xtf.dynaXML.DynaXML?source=/BITAGAP/Display/9775BITAGAP.Work.xml&style=Work.xsl&gobk=https%3A%2F%2Falobag.com">click here</a>
+<a href="httpshttps://pb.lib.berkeley.edu/xtf/servlet/org.cdlib.xtf.dynaXML.DynaXML?source=/BITAGAP/Display/9775BITAGAP.Work.xml&style=Work.xsl&gobk=https%3A%2F%2Fballoonchi.com">click here</a>
+<a href="httpshttps://clients1.google.de/url?q=http%3A%2F%2Falobag.com%2F">click here</a>
+<a href="httpshttps://clients1.google.de/url?q=http%3A%2F%2Fballoonchi.com%2F">click here</a>
+<a href="httpshttps://clients1.google.de/url?q=http%3A%2F%2Fdibapart.com%2F">click here</a>
+<a href="httpshttps://clients1.google.de/url?q=http%3A%2F%2Fcooltech.cool%2F">click here</a>
+<a href="httpshttps://remote.sdc.gov.on.ca/_layouts/15/Gov.ON.LTC.SSE.Extranet.Authentication/forgotpassword.aspx?ci=en-US&sb=https%3A%2F%2Falobag.com">click here</a>
+<a href="httpshttps://remote.sdc.gov.on.ca/_layouts/15/Gov.ON.LTC.SSE.Extranet.Authentication/forgotpassword.aspx?ci=en-US&sb=https%3A%2F%2Fdibapart.com">click here</a>
+<a href="httpshttps://remote.sdc.gov.on.ca/_layouts/15/Gov.ON.LTC.SSE.Extranet.Authentication/forgotpassword.aspx?ci=en-US&sb=https%3A%2F%2Fballoonchi.com">click here</a>
+<a href="httpshttps://remote.sdc.gov.on.ca/_layouts/15/Gov.ON.LTC.SSE.Extranet.Authentication/forgotpassword.aspx?ci=en-US&sb=https%3A%2F%2Fcooltech.cool">click here</a>
+<a href="httpshttp://propelabu.org/w/index.php?majaleh">click here</a>
+<a href="https">click here</a>
+<a href="http://majalesanat.parsiblog.com/">click here</a>
+<a href="https://cool78bag.niloblog.com/">click here</a>
+<a href="https://fa.shafaqna.com/news/1687298/%D8%AA%D8%A8%D9%84%DB%8C%D8%BA%D8%A7%D8%AA-%D9%85%D8%B9%D8%B1%D9%81%DB%8C-5-%D8%A8%D8%B1%D9%86%D8%AF-%D8%B4%DB%8C%D8%B1-%D8%A7%D9%86%D8%A8%D8%B3%D8%A7%D8%B7-%D8%A7%D9%84%DA%A9%D8%AA%D8%B1%D9%88%D9%86/">click here</a>
+<a href="https://www.khordad.news/fa/news/393168/%D9%85%D8%B5%D8%B1%D9%81-%D8%A8%D8%B1%D9%82-%D8%AF%D8%A7%DA%A9%D8%AA-%D8%A7%D8%B3%D9%BE%DB%8C%D9%84%D8%AA-%D8%A8%DB%8C%D8%B4%D8%AA%D8%B1-%D8%A7%D8%B3%D8%AA-%DB%8C%D8%A7-%DA%86%DB%8C%D9%84%D8%B1">click here</a>
+<a href="https://users.sussex.ac.uk/~tj86/home-chiller.html">click here</a>
+<a href="https://techiran55.blogpayz.com/">click here</a>
+<a href="https://alobag.thezenweb.com/">click here</a>
+<a href="https://part67589.ampedpages.com/">click here</a>
+<a href="https://net76783.tinyblogging.com/">click here</a>
+<a href="https://netpart7687577.develop-blog.com/">click here</a>
+<a href="https://alonet98987.mybjjblog.com/">click here</a>
+<a href="https://part62di.activoblog.com/">click here</a>
+<a href="https://part98976.pages10.com/">click here</a>
+<a href="https://net98976.shotblogs.com/">click here</a>
+<a href="https://part76887.worldblogged.com/">click here</a>
+<a href="https://part878798.qowap.com/">click here</a>
+<a href="https://net910876.bloguetechno.com/">click here</a>
+<a href="https://servicepart10.pointblog.net/">click here</a>
+<a href="https://www.quia.com/profiles/dipart">click here</a>
+<a href="https://artmight.com/user/profile/3033333">click here</a>
+<a href="https://www.im-creator.com/free/aminsami/part1092">click here</a>
+<a href="www.protopage.com/part1234">click here</a>
+<a href="https://www.protopage.com/cooltech">click here</a>
+<a href="https://speakerdeck.com/techhvac/khysh-prchhy-rzn-y-grn">click here</a>
+<a href="https://speakerdeck.com/techhvac/hmyt-stfdh-z-gyj-mnyfwld-dyjytl">click here</a>
+<a href="https://cooltech.gitbook.io/untitled/">click here</a>
+<a href="https://cooltech.gitbook.io/dybapart/">click here</a>
+<a href="https://cooltech.gitbook.io/kwl-tk/">click here</a>
+<a href="https://part4578.farsiblog.com/">click here</a>
+<a href="https://part7689.loxblog.com/">click here</a>
+<a href="https://part7689.glxblog.com/">click here</a>
+<a href="http://www.part7689.loxtarin.com/">click here</a>
+<a href="http://www.part7689.mahtarin.com/">click here</a>
+<a href="https://intensedebate.com/people/balloonchi">click here</a>
+<a href="https://aminsami91.educatorpages.com/">click here</a>
+<a href="https://www.provenexpert.com/dibapart/">click here</a>
+<a href="https://www.provenexpert.com/cooltech/">click here</a>
+<a href="http://krachelart.com/UserProfile/tabid/43/userId/1255519/Default.aspx">click here</a>
+<a href="https://www.provenexpert.com/alobag/">click here</a>
+<a href="http://krachelart.com/UserProfile/tabid/43/userId/1255521/Default.aspx">click here</a>
+<a href="http://krachelart.com/UserProfile/tabid/43/userId/1255522/Default.aspx">click here</a>
+<a href="https://atlas.dustforce.com/user/cooltech">click here</a>
+<a href="https://atlas.dustforce.com/user/alobag">click here</a>
+<a href="https://atlas.dustforce.com/user/dibapart">click here</a>
+<a href="http://part324.avablog.ir/">click here</a>
+<a href="https://environmentaltest.blog.ir/">click here</a>
+<a href="https://hadiehtablighati.blog.ir/">click here</a>
+<a href="https://www.midi.org/forum/profile/142493-cooltech">click here</a>
+<a href="https://friendica.eskimo.com/profile/cooltech">click here</a>
+<a href="https://friendica.eskimo.com/profile/dibapart">click here</a>
+<a href="https://devfolio.co/@cooltech">click here</a>
+<a href="https://devfolio.co/@Dibapart">click here</a>
+<a href="https://devfolio.co/@alobag">click here</a>
+<a href="https://friendica.eskimo.com/profile/alobag">click here</a>
+<a href="https://community.wongcw.com/dibapart">click here</a>
+<a href="https://community.wongcw.com/cooltech">click here</a>
+<a href="https://community.wongcw.com/alobag">click here</a>
+<a href="https://www.undrtone.com/dibapart">click here</a>
+<a href="https://www.undrtone.com/Cooltech">click here</a>
+<a href="https://www.undrtone.com/Alobag">click here</a>
+       
       </main>
 
       <Footer />
